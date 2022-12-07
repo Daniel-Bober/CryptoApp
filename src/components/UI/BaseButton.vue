fa054d37372fd38bd2f1d4a76e0f80e8a9bba528
Daniel <99998739+Daniel-Bober@users.noreply.github.com>
2022-12-07 20:07:39 +0100

    feat: init BaseButton comp

---
+++ src/components/UI/BaseButton.vue
@@ -0,0 +1,37 @@
+<template>
+  <button :class="mode">
+    <slot></slot>
+  </button>
+</template>
+
+<script lang='ts'>
+export default {
+  props: {
+    mode: String
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+button {
+  min-height: 40px;
+  padding: 0.5rem 1rem;
+  font-family: 'sf-pro-display-semibold', serif;
+  font-size: $font-14px;
+  color: white;
+  background-color: $purple;
+  border-radius: 4px;
+}
+
+.clear {
+  color: $purple;
+  border: 1px solid $light-gray;
+  background-color: white;
+}
+
+.margin-right-8px {
+  margin-right: 8px;
+}
+
+
+</style>
