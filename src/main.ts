17ad4558856f9e826ca764532a3d7bde6cbd61f1
Daniel <99998739+Daniel-Bober@users.noreply.github.com>
2022-12-07 20:13:20 +0100

    feat: import main scss and BaseButton comp globally

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
-
-import './assets/CSS/main.css'
+import './assets/scss/main.scss'
+import BaseButton from "@/components/UI/BaseButton.vue";
 
 const app = createApp(App)
+
+app.component('BaseButton', BaseButton)
 
 app.use(createPinia())
 
