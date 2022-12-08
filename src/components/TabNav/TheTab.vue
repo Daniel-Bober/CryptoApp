0065a4c33734253b0e0dd3a20d6d27981fdd3d34
Daniel <99998739+Daniel-Bober@users.noreply.github.com>
2022-12-08 23:55:35 +0100

    feat: init TheTab comp

---
+++ src/components/TabNav/TheTab.vue
@@ -0,0 +1,27 @@
+<template>
+<div class="tab-content" v-show="selectedTab === title">
+  <slot></slot>
+</div>
+</template>
+
+<script setup lang='ts'>
+
+
+import {inject} from "vue";
+
+const props = defineProps({
+  title: {
+    type: String,
+    required: true
+  }
+});
+
+const selectedTab = inject('selectedTab')
+
+</script>
+
+<style lang='scss' scoped>
+
+
+
+</style>
