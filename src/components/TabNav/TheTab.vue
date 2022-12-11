3b2d655d68665e5b95dc1ca43d7d8f701be8256c
Daniel <99998739+Daniel-Bober@users.noreply.github.com>
2022-12-11 18:14:20 +0100

    style: add styles

--- src/components/TabNav/TheTab.vue
+++ src/components/TabNav/TheTab.vue
@@ -1,12 +1,10 @@
 <template>
-<div class="tab-content" v-show="selectedTab === title">
-  <slot></slot>
-</div>
+  <div class="tab-content" v-show="selectedTab === title">
+    <slot/>
+  </div>
 </template>
 
 <script setup lang='ts'>
-
-
 import {inject} from "vue";
 
 const props = defineProps({
@@ -16,12 +14,13 @@ const props = defineProps({
   }
 });
 
-const selectedTab = inject('selectedTab')
-
+const selectedTab = inject('selectedTab');
 </script>
 
 <style lang='scss' scoped>
-
-
-
+.tab-content {
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
+}
 </style>
